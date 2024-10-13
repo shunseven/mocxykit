437940dcc83d9c0df1575256ca1386fac5030130
seven.zhong <seven.zhong@vipshop.com>
2024-10-13 15:02:53 +0800

    feat: 添加匹配逻辑

--- server.ts
+++ server.ts
@@ -1,23 +1,16 @@
-/**
- * Created by seven on 16/3/18.
- */
-var express=require('express');
-var app=express();
-var proxyMock=require('./src/index');
-proxyMock(app,{configPath:'/', disabled: ['setCode']})
+import express from 'express';
+import { mockProxy } from './main/index';
 
-var http = require('http');
-var server = http.createServer(app);
-server.listen(3008, '0.0.0.0', function(err: any) {
-    if (err) throw err;
-    var addr = server.address();
-    console.log('Listening at http://%s:%d', addr.address, addr.port);
-});
 
-module.exports = app
-process.on('uncaughtException', function (err: any) {
-    //打印出错误
-    console.log(err);
-    //打印出错误的调用栈方便调试
-    console.log(err.stack);
-});
+async function createServer() {
+  const app = express();
+  mockProxy(app, {
+    apiRule: '/api/*'
+  })
+  // 创建 Vite 服务器
+  app.listen(8800, () => {
+    console.log('Server is running at http://localhost:8800');
+  });
+}
+
+createServer();
