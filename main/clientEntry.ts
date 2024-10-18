19b87034c95e6b108f6ea1454b3e1343353df4af
shun <shun@sevendeMacBook-Pro.local>
2024-10-18 18:36:17 +0800

    feat: 修改为 express 插件

---
+++ main/clientEntry.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction, Application } from 'express';
+import path from 'path';
+import { matchRouter } from './mockProxy/common/fun';
+
+var rootPath = path.join(__dirname,'../viewsDist')  
+export default function (options: ProxyMockOptions) {
+  return function (req: Request, res: Response) {
+    if (matchRouter(options.configPath as string, req.path)){
+      res.sendFile(rootPath + '/index.html');
+      return
+    }
+    if (matchRouter('/expressProxyMockAsset*', req.path)){
+      res.sendFile(rootPath + req.url);
+      return
+    }
+  }
+}
